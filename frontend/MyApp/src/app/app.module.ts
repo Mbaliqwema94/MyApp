9f9df41c7821065f068744ba7ea777405959530f
Mbaliqwema94 <129956817+Mbaliqwema94@users.noreply.github.com>
2023-06-03 14:56:16 +0200

    recent

--- frontend/MyApp/src/app/app.module.ts
+++ frontend/MyApp/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    RegisterComponent,
+    LoginComponent,
+    LandingpageComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
