ad984b3ee64fc06d351e639d06bcd1c9cfba0de3
Mbaliqwema94 <129956817+Mbaliqwema94@users.noreply.github.com>
2023-06-07 15:16:35 +0200

    recent

--- frontend/MyApp/src/app/app-routing.module.ts
+++ frontend/MyApp/src/app/app-routing.module.ts
@@ -6,16 +6,18 @@ import { LoginComponent } from './components/login/login.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { RequestsComponent } from './components/requests/requests.component';
 import { ProductServiceService } from './product.service';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AboutUsComponent } from './about-us/about-us.component';
+
 
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'about', component: AboutUsComponent },
+  { path: '', component: HomepageComponent ,pathMatch: 'full'},
   { path: 'Register', component: RegisterComponent},
   { path: 'Login', component: LoginComponent},
-  { path: '**', redirectTo: '', pathMatch: 'full' }// Redirect to home for any other route
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },// Redirect to home for any other route
+  { path: 'app-landingpage',component: LandingpageComponent },
+  { path: 'productServiceService', component: ProductServiceService},
+  { path: 'app-requests', component: RequestsComponent}
+
 ];
 
 @NgModule({
--- frontend/MyApp/src/app/app.module.ts
+++ frontend/MyApp/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsComponent } from './components/products/products.component';
 import { RequestsComponent } from './components/requests/requests.component';
 // import { ChatsComponent } from './components/chats/chats.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
-import { AboutUsComponent } from './about-us/about-us.component';
 // import { AngularFireModule } from '@angular/fire';
 // import { AngularFirestoreModule } from '@angular/fire/firestore';
 
@@ -23,18 +21,18 @@ import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
+   
     AppComponent,
-    ComponentsComponent,
     RegisterComponent,
     LoginComponent,
     LandingpageComponent,
     HomepageComponent,
     FooterComponent,
-    NavbarComponent,
     ProductsComponent,
     RequestsComponent,
+    NavbarComponent
     // ChatsComponent,
-    AboutUsComponent,
+   
     
 
   ],
